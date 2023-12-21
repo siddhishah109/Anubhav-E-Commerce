131448ed1fd9b05ae86387faa394f95a8dc737fe
Siddhi Shah <shahsiddhi777@gmail.com>
2023-12-21 16:10:17 +0530

    react for front end

--- frontend/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
-
-const store = configureStore({
-  reducer: rootReducer,
-  // Add other configurations as needed
-});
-
-export default store;
