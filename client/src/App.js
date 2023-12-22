4fe8f998925e009c920668b5c5a37296b2e54355
Siddhi Shah <shahsiddhi777@gmail.com>
2023-12-22 13:33:12 +0530

    footer, header, home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {Routes, Route, BrowserRouter } from "react-router-dom"
+import NavBar from './components/navBar';
+import Footer from './components/footer';
+import Home from './components/home';
+import About from './components/about';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route path='/about'element={<About/>}/>
+
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+
+const Home = () => {
+  return (
+    <div>
+      <div className='banner1'>
+      <Button className="bannre1-button" variant="contained" >Sign In</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
