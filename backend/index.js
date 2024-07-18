9dcce3f0529126d3089b5c7bf1d943a9204eaec2
Siddhi Shah <shahsiddhi777@gmail.com>
2024-07-18 16:09:02 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : true,
     credentials : true
 }))
 app.use(express.json())
